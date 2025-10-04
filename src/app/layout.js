f9bb9df2634fdfffa6fbde094728a1fc2931272d
bouncerdesign-beep <bouncerdesign@gmail.com>
2025-10-05 02:17:44 +0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -73,12 +73,27 @@ export default function RootLayout({ children }) {
   return (
     <html lang="tr">
       <head>
+    
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5WZJXT2F');</script>
+<!-- End Google Tag Manager -->
+    
         <link rel="preconnect" href="https://www.googletagmanager.com" />
         <link rel="preconnect" href="https://www.google-analytics.com" />
         <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
         <link rel="dns-prefetch" href="https://www.google-analytics.com" />
       </head>
       <body className={inter.className}>
+    
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WZJXT2F"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+  
         <Analytics />
         <JsonLd />
         {children}
