6469cdb65e6e92ff0ee7defe1f7bbc733f88ed0b
bouncerdesign-beep <bouncerdesign@gmail.com>
2025-10-05 02:20:00 +0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -81,11 +81,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-5WZJXT2F');</script>
 <!-- End Google Tag Manager -->
-    
-        <link rel="preconnect" href="https://www.googletagmanager.com" />
-        <link rel="preconnect" href="https://www.google-analytics.com" />
-        <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
-        <link rel="dns-prefetch" href="https://www.google-analytics.com" />
       </head>
       <body className={inter.className}>
     
